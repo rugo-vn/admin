f7584be012f89b70cd6ccd08621f6e82eb8a2dc2
Vu Anh Hao <hi@haova.me>
2023-01-29 09:59:53 +0700

    Prevent render before load info

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,27 @@
 import ScreenLoader from "./components/ScreenLoader.vue";
 import NoticeDisplayer from "./components/NoticeDisplayer.vue";
 import { useApiStore } from "./stores/api.js";
+import { ref } from "vue";
+import RLoader from "./components/RLoader.vue";
 
 const apiStore = useApiStore();
+const isFirstLoad = ref(true);
 
 apiStore.http.init();
-apiStore.load();
+
+const startLoad = async () => {
+  await apiStore.load();
+  isFirstLoad.value = false;
+}
+
+startLoad();
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-if="!isFirstLoad" />
+  <div v-else class="w-full h-full fixed flex justify-center items-center pointer-events-none z-40">
+    <RLoader class="w-[4rem] h-[4rem]" />
+  </div>
   <ScreenLoader v-if="apiStore.http.isLoading" />
   <NoticeDisplayer :notices="apiStore.http.notices" />
 </template>
